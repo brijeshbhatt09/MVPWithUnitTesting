93ea5d087bad455d76e2eb082edd291f98fbcf46
Brijesh.Bhatt <Brijesh.bhatt@ril.com>
2018-12-11 17:00:04 +0530

    ListActivity and activity_list layout updated to add fragment

--- app/src/main/java/com/brijesh/testapp/ui/ListActivity.java
+++ app/src/main/java/com/brijesh/testapp/ui/ListActivity.java
@@ -1,36 +1,24 @@
 package com.brijesh.testapp.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.brijesh.testapp.R;
-import com.brijesh.testapp.data.AppDataManager;
-import com.brijesh.testapp.model.Rows;
-import com.brijesh.testapp.model.ViewResponse;
-import com.brijesh.testapp.ui.adapter.DataViewAdapter;
-import com.brijesh.testapp.ui.interfaces.MainMVP;
 
-import java.util.ArrayList;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * Created by ${Brijesh.Bhatt} on 10/12/18.
  */
 
-public class ListActivity extends AppCompatActivity implements MainMVP.View
+public class ListActivity extends AppCompatActivity
 {
-    /*Declared objects and bind view with Resources*/
+    private static final String TAG = ListActivity.class.getSimpleName();
 
-    @BindView(R.id.recyclerView) RecyclerView recyclerView;
-
-    private RecyclerView.LayoutManager layoutManager;
-    private DataViewAdapter dataViewAdapter;
-    private MainMVP.Presenter presenter;
-    private ArrayList<Rows> rowsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,12 +27,9 @@ public class ListActivity extends AppCompatActivity implements MainMVP.View
         setContentView(R.layout.activity_list);
 
         /*Bind view this activity to butterknife*/
-
         ButterKnife.bind(this);
 
-        /*initialize Vies and Presenter*/
-        initView();
-        initPresenter();
+        initFragment();
     }
 
     @Override
@@ -57,52 +42,61 @@ public class ListActivity extends AppCompatActivity implements MainMVP.View
     protected void onPause()
     {
         super.onPause();
-        presenter.onDetach(this);
     }
 
-    private void initView()
+    /*Attach Fragment to Activity*/
+    private void initFragment()
     {
-        /* This will create empty list, adapter  and set adapter to recycler*/
-
-        rowsList = new ArrayList<>();
-        dataViewAdapter = new DataViewAdapter(rowsList);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(dataViewAdapter);
+        Fragment replacableFragment = new ListActivityFragment();
+        replaceContentFragment(getSupportFragmentManager(), replacableFragment, true, R.id.fragmentContainer, 0, 0, 0, 0, false);
     }
 
+    /**
+     * Replaces a fragment on activity
+     *
+     * @param fragment       The fragment to show
+     * @param containerId    Container ID in layout where fragment needs to be shown
+     * @param enterAnim
+     * @param exitAnim
+     * @param popEnterAnim
+     * @param popExitAnim
+     * @param addToBackStack true if Back button should empty the fragment | false if otherwise
+     */
 
-    private void initPresenter()
+    public void replaceContentFragment(FragmentManager fragmentManager, Fragment fragment, boolean toAdd, int containerId, int enterAnim, int exitAnim, int popEnterAnim, int popExitAnim, boolean addToBackStack)
     {
-        /*intialize presenter object, attach view to presenter and call service to fetch data from server*/
 
-        presenter = new ListActivityPresenter(AppDataManager.getInstance(), this);
-        presenter.onAttach(this);
-        presenter.callWebService("https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json");
-    }
-
-    @Override
-    public void updateResponse(ViewResponse viewResponse)
-    {
-        /* update view on service response*/
-
-        if(viewResponse != null)
+        /*Return if fragment to be added BaseUIActivity.javais null*/
+        if (fragment == null)
         {
-            rowsList.addAll(viewResponse.getRows());
-            dataViewAdapter.notifyDataSetChanged();
+            throw new IllegalArgumentException("Null fragment passed in " + TAG + "#replaceContentFragment");
         }
-    }
 
-    @Override
-    public void showLoading()
-    {
+        /*getting the name of the fragment class*/
+        String mFragmentName = ((Object) fragment).getClass().getSimpleName();
 
-    }
 
-    @Override
-    public void hideLoading()
-    {
+        /* replace with new content*/
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+        /*Animation for above ICS*/
+        fragmentTransaction.setCustomAnimations(enterAnim, exitAnim, popEnterAnim, popExitAnim);
+        if (toAdd)
+        {
+            fragmentTransaction.add(containerId, fragment, mFragmentName);
+
+        } else
+        {
+            fragmentTransaction.replace(containerId, fragment, mFragmentName);
+        }
+
+        if (addToBackStack)
+        {
+            fragmentTransaction.addToBackStack(mFragmentName);
+        }
+
+        fragmentTransaction.commit();
+        Log.e("TAG", "Success");
     }
 
     @Override
