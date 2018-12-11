d40c0bf52593020cecf20bdfcb197da0ed38be8b
Brijesh.Bhatt <Brijesh.bhatt@ril.com>
2018-12-11 18:07:23 +0530

    Added classes for Network Check and Caching

---
+++ app/src/main/java/com/brijesh/testapp/utils/NetworkCheck.java
@@ -0,0 +1,35 @@
+package com.brijesh.testapp.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by ${Brijesh.Bhatt} on 11/12/18.
+ */
+
+/*This class is responsible to check Network Availability*/
+
+public class NetworkCheck
+{
+    private static ConnectivityManager cm;
+
+    public static void initNetwork(Context context)
+    {
+        cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    public static boolean isOnline()
+    {
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        if (netInfo != null)
+        {
+            return netInfo.isConnected(); // WIFI connected
+        } else
+        {
+            return false;
+        }
+
+    }
+}
