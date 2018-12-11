186544206f85386f5ca8519a746da9f19b47295d
Brijesh.Bhatt <Brijesh.bhatt@ril.com>
2018-12-11 18:10:49 +0530

    updated glide updated glide

--- app/src/main/java/com/brijesh/testapp/ui/holder/DataViewHolder.java
+++ app/src/main/java/com/brijesh/testapp/ui/holder/DataViewHolder.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.brijesh.testapp.R;
 import com.brijesh.testapp.model.Rows;
 import com.bumptech.glide.Glide;
