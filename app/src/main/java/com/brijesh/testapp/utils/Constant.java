6fc23c74a60c03b905bc6e1da9b15b6c4953f11c
Brijesh.Bhatt <Brijesh.bhatt@ril.com>
2018-12-24 11:17:39 +0530

    Constant file added to add constant in application, default image drawable added and Fragment Test class added

---
+++ app/src/main/java/com/brijesh/testapp/utils/Constant.java
@@ -0,0 +1,17 @@
+package com.brijesh.testapp.utils;
+
+/**
+ * Created by ${Brijesh.Bhatt} on 22/12/18.
+ */
+public final class Constant
+{
+    //Api Urls
+    public static  String BASE_URL = "https://dl.dropboxusercontent.com/";
+    public static final String HOME_API_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json";
+
+    /* Api Faliure errors*/
+    public static final String ERROR = "Something went wrong please refresh to try again.";
+    public static final String ERROR_TIMEOUT = "Timeout please refresh to try again.";
+    public static final String ERROR_CONNECTION = "Fail to connect to server. please refresh to try again.";
+
+}
