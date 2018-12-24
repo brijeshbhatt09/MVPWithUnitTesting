b6497ce12819a5e9a0031abab4a6b32e4a2de4c7
Brijesh.Bhatt <Brijesh.bhatt@ril.com>
2018-12-24 11:16:20 +0530

    Json file added to show test response and RestServiceTestHelper class added to read json

---
+++ app/src/androidTest/java/com/brijesh/testapp/utils/RestServiceTestHelper.java
@@ -0,0 +1,39 @@
+package com.brijesh.testapp.utils;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ${Brijesh.Bhatt} on 24/12/18.
+ */
+
+public class RestServiceTestHelper {
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    public static String getStringFromFile(Context context, String filePath) throws Exception {
+
+        AssetManager am = context.getAssets();
+        InputStream stream = am.open(filePath);
+        //final InputStream stream = context.getResources().getAssets().open(filePath);
+
+        String ret = convertStreamToString(stream);
+        //Make sure you close all streams.
+        stream.close();
+        return ret;
+    }
+}
+
