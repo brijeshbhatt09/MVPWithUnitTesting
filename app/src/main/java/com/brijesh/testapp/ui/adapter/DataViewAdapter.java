70cf5f416b5b21ab939e8d85b7c5e5b99da499a1
Brijesh.Bhatt <Brijesh.bhatt@ril.com>
2018-12-24 11:26:02 +0530

    modified for null check

--- app/src/main/java/com/brijesh/testapp/ui/adapter/DataViewAdapter.java
+++ app/src/main/java/com/brijesh/testapp/ui/adapter/DataViewAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.brijesh.testapp.R;
 import com.brijesh.testapp.model.Rows;
 import com.brijesh.testapp.ui.holder.DataViewHolder;
--- app/src/main/java/com/brijesh/testapp/ui/holder/DataViewHolder.java
+++ app/src/main/java/com/brijesh/testapp/ui/holder/DataViewHolder.java
@@ -1,10 +1,11 @@
 package com.brijesh.testapp.ui.holder;
 
-import android.graphics.drawable.ColorDrawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.brijesh.testapp.R;
@@ -25,6 +26,7 @@ public class DataViewHolder extends RecyclerView.ViewHolder
     @BindView(R.id.rowTitle) TextView _rowTitle;
     @BindView(R.id.rowDescription) TextView _rowDescription;
     @BindView(R.id.rowImage) ImageView _rowImage;
+    @BindView(R.id.rowContainer) LinearLayout _rowContainer;
 
     public DataViewHolder(View itemView)
     {
@@ -35,12 +37,25 @@ public class DataViewHolder extends RecyclerView.ViewHolder
     /*This method will populate data in all fileds of row and call glide library for image loading*/
     public void bindViewHolder(Rows row)
     {
-        _rowTitle.setText(row.getTitle());
-        _rowDescription.setText(row.getDescription());
+        setData(row.getTitle(), _rowTitle);
+        setData(row.getDescription(), _rowDescription);
         Glide.with(itemView.getContext())
                 .load(row.getImageHref())
                 .thumbnail(0.5f)
-                .apply(new RequestOptions().placeholder(new ColorDrawable(ContextCompat.getColor(itemView.getContext(), R.color.colorPlaceholder))))
+                .apply(new RequestOptions().placeholder((ContextCompat.getDrawable(itemView.getContext(), R.drawable.default_image))))
                 .into(_rowImage);
+    }
+
+
+    private void setData(String data, TextView view)
+    {
+        if(!TextUtils.isEmpty(data))
+        {
+            view.setText(data);
+        }
+        else
+        {
+            view.setText(itemView.getResources().getString(R.string.default_text));
+        }
     }
 }
