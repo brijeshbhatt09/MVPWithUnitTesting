aaababc344b3585e7799f94f6aced6930cd66bcc
Brijesh.Bhatt <Brijesh.bhatt@ril.com>
2018-12-24 11:25:01 +0530

    interface added to update toolbar title in activity

---
+++ app/src/main/java/com/brijesh/testapp/ui/interfaces/UpdateTitleListener.java
@@ -0,0 +1,12 @@
+package com.brijesh.testapp.ui.interfaces;
+
+/**
+ * Created by ${Brijesh.Bhatt} on 22/12/18.
+ */
+
+/*This will notify ListActivity for any title update*/
+
+public interface UpdateTitleListener
+{
+    public void onTitleUpdateCallback(String title);
+}
