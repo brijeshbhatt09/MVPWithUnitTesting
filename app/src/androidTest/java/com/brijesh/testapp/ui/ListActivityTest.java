c8d49a34465d8dde004060eb6d098dce7dde9569
Brijesh.Bhatt <Brijesh.bhatt@ril.com>
2018-12-24 11:24:18 +0530

    modified to show toolbar title

--- app/src/androidTest/java/com/brijesh/testapp/ui/ListActivityTest.java
+++ app/src/androidTest/java/com/brijesh/testapp/ui/ListActivityTest.java
@@ -2,10 +2,7 @@ package com.brijesh.testapp.ui;
 
 import android.app.Activity;
 import android.os.Build;
-import android.support.test.filters.LargeTest;
-import android.support.test.filters.RequiresDevice;
 import android.support.test.filters.SdkSuppress;
-import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
 import android.util.Log;
 import android.view.View;
@@ -42,53 +39,16 @@ public class ListActivityTest
 
     }
 
-    @Test
-    public void TestAllData(){
-        View view = mActivity.findViewById(R.id.fragmentContainer);
-        assertNotNull(view);
-    }
-
     @After
     public void tearDown() throws Exception {
         mActivity = null;
     }
 
     @Test
-    @RequiresDevice
-    public void testRequiresDevice() {
-        Log.d("Test Filters", "This test requires a device");
-        ListActivity ListActivity = mActivityTestRule.getActivity();
-        assertNotNull("ListActivity is not available", ListActivity);
-
-    }
-    @Test
-    @SdkSuppress(minSdkVersion = 19)
-    public void testMinSdkVersion() {
-        Log.d("Test Filters", "Checking for min sdk >= 19");
-        ListActivity ListActivity = mActivityTestRule.getActivity();
-        assertNotNull("ListActivity is not available", ListActivity);
-    }
-    @Test
     @SdkSuppress(minSdkVersion = Build.VERSION_CODES.KITKAT)
     public void testMinBuild() {
         Log.d("Test Filters", "Checking for min build > kitkat");
         Activity activity = mActivityTestRule.getActivity();
         assertNotNull("ListActivity is not available", activity);
-    }
-
-    @Test
-    @SmallTest
-    public void testSmallTest() {
-        Log.d("Test Filters", "this is a small test");
-        ListActivity ListActivity= mActivityTestRule.getActivity();
-        assertNotNull("ListActivity is not available", ListActivity);
-    }
-
-    @Test
-    @LargeTest
-    public void testLargeTest() {
-        Log.d("Test Filters", "This is a large test");
-        ListActivity ListActivity = mActivityTestRule.getActivity();
-        assertNotNull("ListActivity is not available", ListActivity);
     }
 }
